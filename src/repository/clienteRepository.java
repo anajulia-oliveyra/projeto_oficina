9b702aeb337ef7a1620b7a2c5468072c0e35710e
Ana-Carolina-Pereira <anacarolina53544@yahoo.com.br>
2025-04-29 16:43:28 -0300

    Atualiza classes Cliente, Repository e View

--- src/repository/clienteRepository.java
+++ src/repository/clienteRepository.java
@@ -4,10 +4,30 @@
  */
 package repository;
 
+import java.util.ArrayList;
+import java.util.List;
+import model.Cliente;
 /**
  *
  * @author ADM
  */
-public class clienteRepository {
+    public class clienteRepository {
+    private List<Cliente> clientes = new ArrayList<>();
     
+    public void adicionarCliente(Cliente cliente) {
+        clientes.add(cliente);
+    }
+    
+    public List<Cliente> listarClientes(){
+        return clientes;
+    }
+    
+    public Cliente buscarClientePorNome(String nome){
+        for (Cliente cliente : clientes){
+            if (cliente.getNome().equalsIgnoreCase(nome)){
+                return cliente;
+            }
+        }
+        return null;
+    }
 }
--- src/view/clienteView.java
+++ src/view/clienteView.java
@@ -4,10 +4,35 @@
  */
 package view;
 
-/**
- *
- * @author ADM
- */
+import model.Cliente;
+import repository.clienteRepository;
+
 public class clienteView {
-    
+
+public class main {
+    public static void main(String[] args) {
+
+        Cliente cliente = new Cliente(
+            "Ana Carolina",
+            "Rua quatro, 123",
+            "(38) 99999-9999",
+            "ana@email.com",
+            "123.456.789-00"
+        );
+        
+        System.out.println(cliente);
+        
+        clienteRepository repositorio = new clienteRepository();
+        
+        Cliente cliente1 = new Cliente("Ana Carolina", "Rua for", "(38) 99999-9999", "ana@email.com", "123.456.789-00");
+        Cliente cliente2 = new Cliente("Ana Julia", "Rua B", "(38) 98888-8888", "julia@email.com", "987.654.321-00");
+        
+        repositorio.adicionarCliente(cliente1);
+        repositorio.adicionarCliente(cliente2);
+        
+         for (Cliente c : repositorio.listarClientes()) {
+            System.out.println(c);
+        }
+}
+}
 }
