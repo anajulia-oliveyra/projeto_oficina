c0d8288825e871834af58a960b190bc23cab0cf9
anajulia-oliveyra <anajulia.oliveyra@gmail.com>
2025-05-02 12:02:25 -0300

    criei a classe pra controle de ponto dos funcionarios

--- src/funcionario/ControlePonto.java
+++ src/funcionario/ControlePonto.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package cliente;
+package funcionario;
 
 /**
  *
  * @author ADM
  */
-public class Agendamento {
+public class ControlePonto {
     
 }
