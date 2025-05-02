1ede8a9e05e178b90a2573afacfb1b3761a98d75
Ana-Carolina-Pereira <anacarolina53544@yahoo.com.br>
2025-05-01 21:35:05 -0300

    pagamento

--- src/pagamento/Pagamento.java
+++ src/pagamento/Pagamento.java
@@ -1,13 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package pagamento;
 
-/**
- *
- * @author ADM
- */
+import java.time.LocalDate;
+
 public class Pagamento {
+    private int id;
+    private double valor;
+    private String forma;
+    private boolean pago;
+    private LocalDate dataPagamento;
+    
+    public Pagamento(int id, double valor, String forma){
+        this.id = id;
+        this.valor = valor;
+        this.forma = forma;
+        this.pago = false;
+        this.dataPagamento = null;
+    }
+    
+    public void realizarPagamento(){
+        this.pago = true;
+        this.dataPagamento = LocalDate.now();
+        System.out.println("Pagamento realizado!");
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public String getForma() {
+        return forma;
+    }
+
+    public boolean isPago() {
+        return pago;
+    }
+
+    public LocalDate getDataPagamento() {
+        return dataPagamento;
+    }
+
     
 }
