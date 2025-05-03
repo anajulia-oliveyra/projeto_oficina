09d80a987e5d7ce43aca0b1c18dcc60119aae160
anajulia-oliveyra <anajulia.oliveyra@gmail.com>
2025-05-03 17:57:28 -0300

    Update Login.java

--- src/auth/Login.java
+++ src/auth/Login.java
@@ -7,36 +7,32 @@ package auth;
  */
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 
-import java.io.FileReader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.FileInputStream;
+import java.io.InputStream;
 
 public class Login {
-    public static JSONObject autenticar(String email, String senha)
-    {
-        try 
-        {
-            String conteudo = new String(Files.readAllBytes(Paths.get("usuarios.json")));
-            JSONArray usuarios = new JSONArray(conteudo);
-            
-            for (int i=0; i<usuarios.length(); i++)
-            {
-            JSONObject u = usuarios.getJSONObject(i);
-            if(u.getString("email").equalsIgnoreCase(email) && 
-                u.getString("senha").equals(senha)) 
-                {
-                    return u;
-                }
 
+    private JSONArray usuarios;
+
+    public Login(String caminhoJson) {
+        try (InputStream is = new FileInputStream(caminhoJson)) {
+            JSONTokener tokener = new JSONTokener(is);
+            usuarios = new JSONArray(tokener);
+        } catch (Exception e) {
+            System.out.println("Erro ao ler JSON: " + e.getMessage());
+            usuarios = new JSONArray(); // evita null pointer
+        }
+    }
+
+    public JSONObject autenticar(String email, String senha) {
+        for (int i = 0; i < usuarios.length(); i++) {
+            JSONObject usuario = usuarios.getJSONObject(i);
+            if (usuario.getString("email").equals(email) && usuario.getString("senha").equals(senha)) {
+                return usuario;
             }
         }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
         return null;
-        
     }
-    
 }
